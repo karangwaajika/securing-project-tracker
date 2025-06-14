ceb3e18b0a9c78d89b10cf12d9e9fd38c7594657
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-06-14 20:57:21 +0200

    feat: log the user out

--- src/main/java/com/lab/securing_project_tracker/controller/BrowserController.java
+++ src/main/java/com/lab/securing_project_tracker/controller/BrowserController.java
@@ -39,4 +39,15 @@ public class BrowserController {
         return "login";
     }
 
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+            SecurityContextHolder.clearContext();
+        }
+        request.getSession().invalidate();
+        return "logout";
+    }
+
 }
