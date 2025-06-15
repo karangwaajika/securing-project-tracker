e032ef677362d9432a7710a163515f2f5d1eb701
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-06-15 11:39:31 +0200

    feat: create token upon oauth successful authentication

---
+++ src/main/java/com/lab/securing_project_tracker/security/oauth/OAuth2AuthenticationSuccessHandler.java
@@ -0,0 +1,67 @@
+package com.lab.securing_project_tracker.security.oauth;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lab.securing_project_tracker.repository.UserRepository;
+import com.lab.securing_project_tracker.security.jwt.JwtUtil;
+import com.lab.securing_project_tracker.util.Role;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public OAuth2AuthenticationSuccessHandler(JwtUtil jwtUtil,
+                                              UserRepository userRepository) {
+        this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        FilterChain chain,
+                                        Authentication authentication)
+            throws IOException, ServletException {
+        AuthenticationSuccessHandler.super.onAuthenticationSuccess(request,
+                response, chain, authentication);
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest  req,
+                                        HttpServletResponse res,
+                                        Authentication      auth) throws IOException {
+
+        OAuth2User principal = (OAuth2User) auth.getPrincipal();
+        String email = principal.getAttribute("email");
+
+        // role from local DB
+        Role role = userRepository.findByEmail(email).get().getRole();
+        // create a userDetails for token creation
+        UserDetails userDetails = new User(
+                email,                   // username → put your e‑mail here
+                "",                      // password not needed for token creation
+                List.of(new SimpleGrantedAuthority("ROLE_" + role.name()))
+        );
+        String token = jwtUtil.generateToken(userDetails);
+
+        // Redirect with token in URL
+         res.sendRedirect("/#/oauth-success?token=" + token);
+    }
+
+}
